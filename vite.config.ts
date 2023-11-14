c2def3b92b4320b6139c3a132ca7dc2a1f250b9e
sonylomo <sonylomo1@gmail.com>
2023-11-14 20:11:40 +0300

    build: intial PWA set-up

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto", //register service worker
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"], // service worker precache (resources to be downloaded for offline access)
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
+});
