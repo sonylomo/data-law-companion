8b1997dd292aaeaf65773593d9946557297982a8
sonylomo <sonylomo1@gmail.com>
2023-11-16 16:48:58 +0300

    style: footer responsive on mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,15 +7,15 @@ const Footer = () => {
   const year = now.getFullYear();
 
   return (
-    <footer className="bg-neutral-grey text-neutral-orange md:p-12 py-8 px-4 relative">
+    <footer className="bg-neutral-grey text-neutral-orange md:p-12 py-8 px-4">
       <div className="flex md:flex-row flex-col justify-between w-2/3 mx-auto">
         {/* Footer links */}
-        <div className="flex flex-col gap-8">
+        <div className="flex flex-col sm:gap-8 gap-0 mt-4 sm:mt-0">
           <h2 className="text-[#9DA0AC] font-normal text-lg leading-9">
             DATA PROTECTION ASSISTANT
           </h2>
 
-          <ul className="flex flex-col gap-5 list-none text-sm font-medium">
+          <ul className="flex flex-col gap-2 sm:gap-5 list-none text-sm font-medium">
             <li>
               <a href="#">Summarization</a>
             </li>
@@ -25,12 +25,12 @@ const Footer = () => {
           </ul>
         </div>
 
-        <div className="flex flex-col gap-8">
+        <div className="flex flex-col sm:gap-8 gap-0 mt-4 sm:mt-0">
           <h2 className="text-[#9DA0AC] font-normal text-lg leading-9">
             DATA PROTECTION ACT
           </h2>
 
-          <ul className="flex flex-col gap-5 list-none text-sm font-medium">
+          <ul className="flex flex-col gap-2 sm:gap-5 list-none text-sm font-medium">
             <li>
               <a href="#">DPA-Kenya</a>
             </li>
@@ -46,12 +46,12 @@ const Footer = () => {
           </ul>
         </div>
 
-        <div className="flex flex-col gap-8">
+        <div className="flex flex-col sm:gap-8 gap-0 mt-4 sm:mt-0">
           <h2 className="text-[#9DA0AC] font-normal text-lg leading-9">
             ABOUT
           </h2>
 
-          <ul className="flex flex-col gap-5 list-none text-sm font-medium">
+          <ul className="flex flex-col gap-2 sm:gap-5 list-none text-sm font-medium">
             <li>
               <a href="#">Team</a>
             </li>
@@ -67,12 +67,12 @@ const Footer = () => {
           </ul>
         </div>
 
-        <div className="flex flex-col gap-8">
+        <div className="flex flex-col sm:gap-8 gap-0 mt-4 sm:mt-0">
           <h2 className="text-[#9DA0AC] font-normal text-lg  leading-9">
             RESOURCES
           </h2>
 
-          <ul className="flex flex-col gap-5 list-none text-sm font-medium">
+          <ul className="flex flex-col gap-2 sm:gap-5 list-none text-sm font-medium">
             <li>
               <a href="#">Case Studies</a>
             </li>
@@ -89,11 +89,11 @@ const Footer = () => {
         </div>
       </div>
       <div className="border-t border-[#9DA0AC] w-2/3 mt-5 sm:mt-10 mx-auto" />
-      <div className="sm:mt-10 mt-5 items-start ">
+      <div className="sm:mt-10 mt-5 items-start w-2/3 mx-auto">
         <h1 className="text-center text-neutral-red my-4 md:my-8">
           &copy; {year} Data Protection Assistant | All Rights Reserved
         </h1>
-        <div className="flex-3 flex flex-col md:items-center items-start">
+        <div className="flex-3 flex flex-col items-center">
           {/* logo */}
           <img src={logo} alt="logo" className=" md:ml-0 ml-4 object-contain" />
         </div>
