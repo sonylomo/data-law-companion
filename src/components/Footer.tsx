5e4fcd973c0b74cf8062ef7f45226a5026f83cd5
sonylomo <sonylomo1@gmail.com>
2023-11-16 16:40:39 +0300

    style: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,56 +8,88 @@ const Footer = () => {
 
   return (
     <footer className="bg-neutral-grey text-neutral-orange md:p-12 py-8 px-4 relative">
-      <div className="flex md:flex-row flex-col justify-evenly">
+      <div className="flex md:flex-row flex-col justify-between w-2/3 mx-auto">
         {/* Footer links */}
         <div className="flex flex-col gap-8">
-          <h2 className="text-[#9DA0AC] font-normal sm:text-xl text-base leading-9">
-            Company
+          <h2 className="text-[#9DA0AC] font-normal text-lg leading-9">
+            DATA PROTECTION ASSISTANT
           </h2>
 
           <ul className="flex flex-col gap-5 list-none text-sm font-medium">
             <li>
-              <Link to="/about-us">About Us</Link>
+              <a href="#">Summarization</a>
             </li>
             <li>
-              <a href="/#faq">FAQs</a>
-            </li>
-            <li>
-              <Link to="/community">Community</Link>
+              <a href="#">Q&A</a>
             </li>
           </ul>
         </div>
 
         <div className="flex flex-col gap-8">
-          <h2 className="text-[#9DA0AC] font-normal sm:text-xl text-lg leading-9">
-            Resources
+          <h2 className="text-[#9DA0AC] font-normal text-lg leading-9">
+            DATA PROTECTION ACT
           </h2>
 
           <ul className="flex flex-col gap-5 list-none text-sm font-medium">
-            <li>Jobs</li>
             <li>
-              <Link to="/allevents">Events</Link>
+              <a href="#">DPA-Kenya</a>
             </li>
-            <li>Mentorship</li>
+            <li>
+              <a href="#">DPA-Rwanda</a>
+            </li>
+            <li>
+              <a href="#">DPA-Uganda</a>
+            </li>
+            <li>
+              <Link to="/compliance-issues">Compliance Issues</Link>
+            </li>
           </ul>
         </div>
 
         <div className="flex flex-col gap-8">
-          <h2 className="text-[#9DA0AC] font-normal sm:text-xl text-lg leading-9">
-            Quick Links
+          <h2 className="text-[#9DA0AC] font-normal text-lg leading-9">
+            ABOUT
           </h2>
 
           <ul className="flex flex-col gap-5 list-none text-sm font-medium">
-            <li>Contact Us</li>
-            <li>Donate</li>
-            <li>Privacy Policy</li>
-            <li>Terms of Use</li>
+            <li>
+              <a href="#">Team</a>
+            </li>
+            <li>
+              <a href="#">Funding & Partners</a>
+            </li>
+            <li>
+              <a href="#">Stakeholders</a>
+            </li>
+            <li>
+              <a href="#">Terms of Use</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="flex flex-col gap-8">
+          <h2 className="text-[#9DA0AC] font-normal text-lg  leading-9">
+            RESOURCES
+          </h2>
+
+          <ul className="flex flex-col gap-5 list-none text-sm font-medium">
+            <li>
+              <a href="#">Case Studies</a>
+            </li>
+            <li>
+              <a href="#">Blog</a>
+            </li>
+            <li>
+              <a href="#">News</a>
+            </li>
+            <li>
+              <a href="#">FAQ</a>
+            </li>
           </ul>
         </div>
       </div>
       <div className="border-t border-[#9DA0AC] w-2/3 mt-5 sm:mt-10 mx-auto" />
       <div className="sm:mt-10 mt-5 items-start ">
-        
         <h1 className="text-center text-neutral-red my-4 md:my-8">
           &copy; {year} Data Protection Assistant | All Rights Reserved
         </h1>
