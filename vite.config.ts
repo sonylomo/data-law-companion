0c1495391f68923c72ba830473dab7a3c82f45fc
sonylomo <sonylomo1@gmail.com>
2024-01-06 15:15:57 +0300

    config: removed base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"https://datalawcompanion.org/",
+  // base:"https://datalawcompanion.org/",
   plugins: [
     react(),
     VitePWA({
